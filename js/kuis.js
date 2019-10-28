32034329cf8a47d974601fadb6c4ea54cd2c5488
ari firmansyah <36336485+arifirmansyah@users.noreply.github.com>
2019-10-28 14:52:59 +0700

    Add files via upload

--- js/kuis.js
+++ js/kuis.js
@@ -68,6 +68,7 @@ function clickStart() {
 
     $('.js-startPage').remove();
     $('#question-container').removeClass('hidden');
+    $('#petunjuk').removeClass('hidden');
   })
 };
 
@@ -79,7 +80,6 @@ function clickAnswer(chosenElement, state) {
   //if the chosen answer is correct, then tell the user "correct", otherwise "wrong :("
   //jika memilik jawaban yang BENAR, maka akan mengatakan pada pengguna "CORRECT" daripada "WRONG"
   if (chosenAnswer == state.questions[state.currentQuestion].answerCorrect) {
-
     state.userScore += 1;
     $('.response1').text('Mantul!');
   } else {
@@ -112,13 +112,14 @@ function clickContinue(state) {
   //increment which question user is on by one when continue is clicked
   state.currentQuestion += 1;
   //hide continue button and result again, remove questions and answer
+  // hide tombol continue dan result, dengan memberikan class hidden
   $('.js-continue').addClass('hidden');
   $('.result').addClass('hidden');
   $('section').remove();
 
-  //kuis berakhir dan menampolkan pesan serta membuang jumlah pertanyaan serta nilai dari halaman bawah
+  //kuis berakhir dan menampilkan pesan serta membuang jumlah pertanyaan serta nilai dari halaman bawah
   if (state.currentQuestion > 9) {
-    $('body').append('<h1 class="end">Anda telah selesai!</h1><p class ="endScore">Skor anda ' + state.userScore + " dari " + state.currentQuestion);
+    $('body').append('<div class="end">Anda telah selesai!</div><p class ="endScore">Skor anda ' + state.userScore + " dari " + state.currentQuestion);
     $('body').append('<a href="index.html" class="balik">Kembali</a>');
     $('.js-count').remove();
     $('.js-score').remove();
@@ -128,10 +129,10 @@ function clickContinue(state) {
     //jika quiz tidak disisipkan pertanyaan baru dan jawaban dan perbarui score pengguna dan perhitungan jumlah pertanyaan yang muncul
     $('#question-container').append("<section class = 'question-container col-8'>" +
       "<p class='question'>" + state.questions[state.currentQuestion].question + "</p><br>" +
-      "<button class='button0 js-answer hover' value = '0'>" + state.questions[state.currentQuestion].answers[0] + "</button><br>" +
-      "<button class='button1 js-answer hover' value = '1'>" + state.questions[state.currentQuestion].answers[1] + "</button><br>" +
-      "<button class='button2 js-answer hover' value = '2'>" + state.questions[state.currentQuestion].answers[2] + "</button><br>" +
-      "<button class='button3 js-answer hover' value = '3'>" + state.questions[state.currentQuestion].answers[3] + "</button>" +
+      "<button class='button button-raised button0 js-answer hover' value = '0'>" + state.questions[state.currentQuestion].answers[0] + "</button><br>" +
+      "<button class='button button-raised button1 js-answer hover' value = '1'>" + state.questions[state.currentQuestion].answers[1] + "</button><br>" +
+      "<button class='button button-raised button2 js-answer hover' value = '2'>" + state.questions[state.currentQuestion].answers[2] + "</button><br>" +
+      "<button class='button button-raised button3 js-answer hover' value = '3'>" + state.questions[state.currentQuestion].answers[3] + "</button>" +
       "</section>");
 
     $('.js-count').text("Soal: " + (state.currentQuestion + 1) + "/" + state.questions.length);
